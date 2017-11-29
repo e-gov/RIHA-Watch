5054cf2149fa22b0f16ba0aa7906eaa7c1960aa0
PriitParmakson <priit.parmakson@gmail.com>
2017-11-30 01:47:44 +0200

    +

--- public/js/Arutelu.js
+++ public/js/Arutelu.js
@@ -27,7 +27,7 @@ function leiaArutelud(systeemideNimekiri) {
         return response.json();
       })
       .then(saadudJSON => {
-        kuvaString(saadudJSON,vastus);
+        kuvaString(saadudJSON.vastus);
       })
       .catch(error => {
         console.log('leiaArutelud: error: ' + error);
