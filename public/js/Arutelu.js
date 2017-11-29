583b4ad886d240d6d77b71e9741edeb6eff7e3b1
PriitParmakson <priit.parmakson@gmail.com>
2017-11-30 00:59:29 +0200

    +

--- public/js/Arutelu.js
+++ public/js/Arutelu.js
@@ -4,34 +4,36 @@ function alusta() {
 
 function leiaSysteemid() {
   fetch('https://apidemojatest.herokuapp.com/systeeme')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(saadudJSON => {
-    $('#Teade').addClass('peidetud');
-    console.log('Andmed saadud');
-    kuvaArutelud(saadudJSON.totalElements);
-    leiaArutelud(saadudJSON.content);
-  })
-  .catch(error => {
-    console.log('Andmete saamine ebaõnnestus');
-  });
-}
-
-function leiaArutelud(systeemideNimekiri) {
-  systeemideNimekiri.forEach(systeem => {
-    /* Päri süsteemiga seotud arutelud */
-    fetch('https://apidemojatest.herokuapp.com/arutelud?s=' + systeem.details.short_name)
     .then(function (response) {
       return response.json();
     })
     .then(saadudJSON => {
-      kuvaArutelud(saadudJSON);
+      $('#Teade').addClass('peidetud');
+      console.log('Andmed saadud');
+      kuvaArutelud(saadudJSON.totalElements);
+      leiaArutelud(saadudJSON.content);
     })
     .catch(error => {
       console.log('Andmete saamine ebaõnnestus');
     });
-  }
+}
+
+function leiaArutelud(systeemideNimekiri) {
+  for (var i = 0; i < systeemideNimekiri.length; i++) {
+    /* Päri süsteemiga seotud arutelud */
+    var u = 'https://apidemojatest.herokuapp.com/arutelud?s=' + systeemideNimekiri[i].details.short_name;
+    console.log(systeemideNimekiri[i].details.short_name);
+    fetch(u)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(saadudJSON => {
+        kuvaArutelud(saadudJSON);
+      })
+      .catch(error => {
+        console.log('Andmete saamine ebaõnnestus');
+      });
+  } 
 }
 
 function kuvaArutelud(json) {
