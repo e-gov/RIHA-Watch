e421bf735bbd8ccb01e2859b96c7bc413a063f6f
PriitParmakson <priit.parmakson@gmail.com>
2017-11-30 02:04:08 +0200

    +

--- index.js
+++ index.js
@@ -51,10 +51,10 @@ app.get('/systeeme', (req, res) => {
 
 /* Süsteemi arutelude päring RIHA-st (AJAX) */
 app.get('/arutelud', (req, res) => {
-  res.status(200)
-    .send('{"vastus":"OK"}');
-  return
   var shortname = req.query.s; 
+  res.status(200)
+    .send('{"vastus":"' + shortname + 'OK"}');
+  return
   requestModule({
     url: 'https://test.riha.ee/api/v1/systems/' + shortName + '/issues',
     method: 'GET'
